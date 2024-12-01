5e8529f89efb4a250ea48e5dc3a47c025c5dcc2d
Yingning7 <60882495+Yingning7@users.noreply.github.com>
2024-12-01 18:16:12 +0000

    Update install_prerequisite.sh

--- bin/install_prerequisite.sh
+++ bin/install_prerequisite.sh
@@ -4,4 +4,3 @@ source ./bin/vars.sh
 
 conda create -n $CONDA_ENV_NAME --file "./bin/$CONDA_REQUIREMENT_FILE" -y
 sudo docker pull "${POSTGRES_IMG_NAME}:${POSTGRES_IMG_TAG}"
-mkdir $POSTGERS_CONTAINER_VOLUME
