542e8a4421b696ea8f5067be23146a670a0ada98
Yingning7 <a199977@qq.com>
2024-12-08 20:09:11 +0000

    create example

--- db.py
+++ db.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 import psycopg2
 
 SCHEMA_NAME = 'shopping_alert'
@@ -48,3 +50,40 @@ def create_table(table_name: str, table_def: dict[str, str], index_def: dict[str
         with conn.cursor() as cursor:
             cursor.execute(query_final + index_query)
         conn.commit()
+
+
+def insert_data(table_name: str, table_def: dict[str, str], df: pd.DataFrame) -> None:
+    df = df.replace({np.nan: None})
+
+    # TODO: Add the INSERT INTO table_name (column1, column2, column3) VALUES
+
+    values_sql_list = []
+    for _, row in df.replace({np.nan: None}).iterrows():
+        single_value_sql_list = []
+        for col_name, dtype in table_def.items():
+            if col_name.startswith('PRIMARY KEY'):
+                continue
+            elif row[col_name] is None:
+                single_value_sql_list.append('NULL')
+            elif dtype.startswith(('TEXT', 'TIME', 'DATE')):
+                single_value_sql_list.append(f"'{row[col_name]}'")
+            elif dtype.startswith('BOOL'):
+                single_value_sql_list.append(f"{row[col_name]}".upper())
+            else:
+                single_value_sql_list.append(str(row[col_name]))
+        values_sql_list.append(f"({', '.join(single_value_sql_list)})")
+    values_sql = f"{',\n'.join(values_sql_list)};"
+
+    # TODO: use conn and cursor, execute the insertion sql, remember to `conn.commit()` 
+    # just like `init_schema` function
+
+    return values_sql  # TODO: remove this return, this is just for debugging the sql string
+
+
+def query_data(query_sql: str, dtypes: dict[str, str]) -> pd.DataFrame:
+    # TODO: use conn and cursor, execute the query_sql and cursor.fetchall(), it will be a list of tuples
+    # TODO: convert the list of tuples to a pandas DataFrame, with column name set
+    # search online / ask ChatGPT, how to convert a list of tuples to a pandas dataframe
+    # and how to get the column names from the psycopg2 after `cursor.fetchall()`
+    # TODO: set the data types for all columns of resultant pandas DataFrame, using the `dtypes` provided
+    pass
