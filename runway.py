6a00017856dde6ad5c1329897d3f336619b97e45
Yingning7 <a199977@qq.com>
2024-12-14 20:16:12 +0000

    removed unnecessary, add df examples

--- runway.py
+++ runway.py
@@ -1,184 +1,100 @@
-import datetime as dt
-import logging
-
-from pydantic import BaseModel
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
 import requests
-import pytz
 
-from base import BaseRecord, BaseScraper
-
-
-class Price(BaseModel):
-    original: float
-    current: float
-    currency: str
-
-
-class Spec(BaseModel):
-    size: str
-    status: str
-
-    @property
-    def is_available(self) -> bool:
-        return self.status != 'SOLD OUT'
-    
-    @property
-    def unit_left(self) -> float:
-        if self.status.startswith('残り') and self.status.endswith('点'):
-            return float(self.status.replace('点', '').replace('残り', ''))
-        return float('NaN')
-
-
-class Detail(BaseModel):
-    color: str
-    specs: list[Spec]
-
-
-class ItemInfo(BaseModel):
-    name: str
-    brand: str
-    price: Price
-    details: list[Detail]
-
-
-class Record(BaseRecord):
-    item_id: str
-    name: str
-    brand: str
-    price_original: float
-    price_current: float
-    price_currency: str
-    color: str
-    size: str
-    is_available: bool
-    unit_left: float
-    asof: dt.datetime
-    url: str
-
-    @classmethod
-    def get_df_dtypes(cls) -> dict[str, str]:
-        return {
-            'item_id': 'str',
-            'name': 'str',
-            'brand': 'str',
-            'price_original': 'float',
-            'price_current': 'float',
-            'price_currency': 'str',
-            'color': 'str',
-            'size': 'str',
-            'is_available': 'bool',
-            'unit_left': 'float',
-            'asof': 'datetime64[ns, UTC]',
-            'url': 'str'
-        }
-    
-    @classmethod
-    def get_db_table_dtypes(cls) -> dict[str, str]:
-        return {
-            'item_id': 'TEXT NOT NULL',
-            'name': 'TEXT NOT NULL',
-            'brand': 'TEXT NOT NULL',
-            'price_original': 'DOUBLE PRECISION NOT NULL',
-            'price_current': 'DOUBLE PRECISION NOT NULL',
-            'price_currency': 'TEXT NOT NULL',
-            'color': 'TEXT NOT NULL',
-            'size': 'TEXT NOT NULL',
-            'is_available': 'BOOLEAN NOT NULL',
-            'unit_left': 'DOUBLE PRECISION',
-            'asof': 'TIMESTAMP WITH TIME ZONE NOT NULL',
-            'url': 'TEXT NOT NULL',
-            'PRIMARY KEY': '(item_id, color, size, asof)'
-        }
-    
-    @classmethod
-    def get_db_index(cls) -> dict[str, str]:
-        return {
-            'item_id': 'item_id',
-            'color': 'color',
-            'size': 'size',
-            'asof': 'asof',
-            'item_id_asof': 'item_id, asof'
-        }
+URL = 'https://runway-webstore.com/ap/item/i/m/{item_id}'
+DTYPES = {
+    'item_id': 'str',
+    'name': 'str',
+    'brand': 'str',
+    'original_price': 'float',
+    'current_price': 'float',
+    'currency': 'str',
+    'color': 'str',
+    'size': 'str',
+    'is_available': 'bool',
+    'unit_left': 'float',
+    'asof': 'datetime64[ns, UTC]',
+    'url': 'str'
+}
+TABLE_NAME = 'runway'
+TABLE_DEF = {
+    'item_id': 'TEXT NOT NULL',
+    'name': 'TEXT NOT NULL',
+    'brand': 'TEXT NOT NULL',
+    'original_price': 'DOUBLE PRECISION NOT NULL',
+    'current_price': 'DOUBLE PRECISION NOT NULL',
+    'currency': 'TEXT NOT NULL',
+    'color': 'TEXT NOT NULL',
+    'size': 'TEXT NOT NULL',
+    'is_available': 'BOOLEAN NOT NULL',
+    'unit_left': 'DOUBLE PRECISION',
+    'asof': 'TIMESTAMP WITH TIME ZONE NOT NULL',
+    'url': 'TEXT NOT NULL',
+    'PRIMARY KEY': '(item_id, color, size, asof)'
+}
+INDEX_DEF = {
+    'idx_item_id': 'item_id',
+    'idx_color': 'color',
+    'idx_size': 'size',
+    'idx_asof': 'asof',
+    'idx_item_id_asof': 'item_id, asof'
+}
 
 
-class Scraper(BaseScraper):
-    URL = 'https://runway-webstore.com/ap/item/i/m/{item_id}'
+def fetch_html(item_id: str) -> str:
+    url = URL.format(item_id=item_id)
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        raise RuntimeError(f'Error when fetching HTML from {url} for Item ID: {item_id}.')
+    html = resp.text
+    return html
 
-    def __init__(self, item_id: str) -> None:
-        self.item_id = item_id
-        self.url = self.URL.format(item_id=item_id)
 
-    def _fetch_html(self) -> str:
-        logging.info(f'Fetching HTML from {self.url}.')
-        resp = requests.get(self.url)
-        if resp.status_code != 200:
-            raise RuntimeError(f'Error when fetching HTML from {self.url} for Item ID: {self.item_id}.')
-        html = resp.text
-        return html
-    
-    @staticmethod
-    def _extract_item_info(html: str) -> ItemInfo:
-        logging.info('Extracting item information from HTML.')
-        soup = BeautifulSoup(html, features='html.parser')
-        name = soup.find('h1',{'class', 'item_detail_productname'}).text.strip()
-        brand = soup.find('p', {'class', 'item_detail_brandname'}).text.strip()
-        if soup.find('p', {'class', 'proper'}):
-            original = soup.find('p', {'class', 'proper'}).text.replace(',', '').replace('円(税込)', '').strip()
-            current = original
-        else:
-            original = soup.find('del').text.replace(',', '').replace('円(税込)', '').strip()
-            current = soup.find('span', {'class', 'sale_price'}).text.replace(',', '').replace('円(税込)', '').strip()
-        price = Price(original=float(original), current=float(current), currency='JPY')
-        detail_ul = soup.find('ul', {'class': 'shopping_area_ul_01'})
-        color_lis = detail_ul.find_all('li', recursive=False)
-        details = []
-        for color_li in color_lis:
-            color = color_li.div.dl.dd.text.strip()
-            specs = []
-            spec_lis = color_li.find('div', {'class': 'choose_item'}).find('ul', {'class': 'shopping_area_ul_02'}).find_all('li', recursive=False)
-            for spec_li in spec_lis:
-                size = spec_li.dt.text.strip()
-                status = spec_li.dd.text.strip()
-                specs.append(Spec(size=size, status=status))
-            details.append(Detail(color=color, specs=specs))
-        item_info = ItemInfo(
-            name=name,
-            brand=brand,
-            price=price,
-            details=details
-        )
-        return item_info
-    
-    def _parse_item_info(self, item_info: ItemInfo, asof: dt.datetime) -> pd.DataFrame:
-        logging.info('Parsing item information to DataFrame.')
-        records = []
-        for detail in item_info.details:
-            for spec in detail.specs:
-                records.append(
-                    Record(
-                        item_id=self.item_id,
-                        name=item_info.name,
-                        brand=item_info.brand,
-                        price_original=item_info.price.original,
-                        price_current=item_info.price.current,
-                        price_currency=item_info.price.currency,
-                        color=detail.color,
-                        size=spec.size,
-                        is_available=spec.is_available,
-                        unit_left=spec.unit_left,
-                        asof=asof,
-                        url=self.url
-                    )
-                )
-        data = pd.DataFrame.from_records([record.dict() for record in records]).astype(Record.get_df_dtypes())
-        return data
+def extract_data(html: str) -> list[dict[str, int | float | str]]:
+    soup = BeautifulSoup(html, features='html.parser')
+    name = soup.find('h1',{'class', 'item_detail_productname'}).text.strip()
+    brand = soup.find('p', {'class', 'item_detail_brandname'}).text.strip()
+    if soup.find('p', {'class', 'proper'}):
+        original_price = soup.find('p', {'class', 'proper'}).text.replace(',', '').replace('円(税込)', '').strip()
+        current_price = original_price
+    else:
+        original_price = soup.find('del').text.replace(',', '').replace('円(税込)', '').strip()
+        current_price = soup.find('span', {'class', 'sale_price'}).text.replace(',', '').replace('円(税込)', '').strip()
+    currency = 'JPY'
+    detail_ul = soup.find('ul', {'class': 'shopping_area_ul_01'})
+    color_lis = detail_ul.find_all('li', recursive=False)
+    data = []
+    for color_li in color_lis:
+        color = color_li.div.dl.dd.text.strip()
+        spec_lis = color_li.find('div', {'class': 'choose_item'}).find('ul', {'class': 'shopping_area_ul_02'}).find_all('li', recursive=False)
+        for spec_li in spec_lis:
+            size = spec_li.dt.text.strip()
+            status = spec_li.dd.text.strip()
+            data.append(
+                {
+                    'name': name,
+                    'brand': brand,
+                    'original_price': original_price,
+                    'current_price': current_price,
+                    'currency': currency,
+                    'color': color,
+                    'size': size,
+                    'status': status
+                }
+            )
+    return data
 
-    def scrape(self) -> pd.DataFrame:
-        asof = pytz.timezone('Europe/London').localize(dt.datetime.now()).astimezone(pytz.UTC)
-        logging.info(f'Scraping at {asof} for Item ID: {self.item_id}.')
-        html = self._fetch_html()
-        item_info = self._extract_item_info(html)
-        data = self._parse_item_info(item_info, asof)
-        return data
+
+def parse_data(item_id: str, data: list[dict[str, str]]) -> pd.DataFrame:
+    df = pd.DataFrame.from_records(data)
+    df['is_available'] = df['status'] != 'SOLD OUT'
+    mask = df['status'].str.startswith('残り') & df['status'].str.endswith('点')
+    df['unit_left'] = np.where(mask, df['status'].str.replace('点', '').str.replace('残り', ''), np.nan)
+    df = df.drop(labels='status', axis=1)
+    df['item_id'] = item_id
+    df['asof'] = pd.Timestamp.now().tz_localize('Europe/London').tz_convert('UTC')
+    df['url'] = URL.format(item_id=item_id)
+    df = df.astype(DTYPES)
+    return df
