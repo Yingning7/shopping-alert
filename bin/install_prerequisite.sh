fdd3969667749ec6daf33a7e175a3a29f382e01c
Yingning7 <a199977@qq.com>
2025-04-20 21:41:09 +0100

    update script

--- bin/install_prerequisite.sh
+++ bin/install_prerequisite.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+set -ex
+
 CONDA_ENV_NAME="shopping-alert"
 CONDA_REQUIREMENT_FILE="conda-requirements.txt"
 
 conda create -n $CONDA_ENV_NAME --file "./bin/$CONDA_REQUIREMENT_FILE" -y
+eval "$(conda shell.bash hook)"
+conda activate $CONDA_ENV_NAME
+pip install playwright
+playwright install
