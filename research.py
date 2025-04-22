3d047d434a5f6618b5fe0da93c09842dfab1b060
Yingning7 <a199977@qq.com>
2025-04-22 21:09:29 +0100

    working

--- research.py
+++ research.py
@@ -6,9 +6,15 @@ if __name__ == '__main__':
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
-        page.goto('https://zozo.jp/shop/cecilmcbee/goods-sale/75511602/?did=123646373')
+        page.goto('https://zozo.jp/shop/mercuryduo/goods-sale/87837454/?did=142436883')
         html = page.content()
         browser.close()
+    
     soup = BeautifulSoup(html, features='html.parser')
     details_div = soup.find('div', {'class': 'cartBlock clearfix', 'data-goods-skus': 'list'})
     print(details_div)
+
+with open('page.html', 'w') as fp:
+    fp.write(html)
+
+
